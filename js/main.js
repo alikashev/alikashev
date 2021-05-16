0904dc72ea4cb688d68e6df3cb6bb4f771043164
Ali Kashev <57992892+alikashev@users.noreply.github.com>
2021-05-16 19:12:55 +0200

    edited animations

--- js/main.js
+++ js/main.js
@@ -76,3 +76,11 @@
         return false;
     });
 })(jQuery);
+
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+    setTimeout(()=>{
+        splash.classList.add('display-none');
+    }, 3000);
+})
