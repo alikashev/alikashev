d3c03701e45b29dad914ef77d34bc40b9a759f46
Ali Kashev <57992892+alikashev@users.noreply.github.com>
2021-08-14 18:45:13 +0200

    Skills + contact aangepast

--- process.php
+++ process.php
@@ -1,5 +1,6 @@
 <?php
 
+
 $errors = [];
 $data = [];
 
@@ -28,5 +29,50 @@ if (!empty($errors)) {
 }
 
 echo json_encode($data);
+
+$to      = 'kasev21@gmailcom';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: webmaster@example.com'       . "\r\n" .
+             'Reply-To: webmaster@example.com' . "\r\n" .
+             'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+
+/*
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+
+$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "kasev21@gmail.com";
+$mail->Password   = "Alliimmeessii11.11";
+
+$mail->IsHTML(true);
+$mail->AddAddress("akshv10@gmail.com", "Kashev Ali");
+$mail->SetFrom("kasev21@gmail.com", "Ali Kashev");
+$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+
+$mail->MsgHTML($content); 
+if(!$mail->Send()) {
+  echo "Error while sending Email.";
+  var_dump($mail);
+} else {
+  echo "Email sent successfully";
+}
+*/
 
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '76c8a287188eb74ce6a3b26b13e23fb6cd01f082',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '76c8a287188eb74ce6a3b26b13e23fb6cd01f082',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.5.0',
+      'version' => '6.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a5b5c43e50b7fba655f793ad27303cd74c57363c',
+    ),
+  ),
+);
