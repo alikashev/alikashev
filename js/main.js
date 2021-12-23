1dba40737113ebccd408bcc1d534510356192087
Ali Kashev <kasev21@gmail.com>
2021-12-24 00:18:36 +0100

    read more added on skills and some small changes

--- js/main.js
+++ js/main.js
@@ -149,3 +149,86 @@ $(document).ready(function () {
       event.preventDefault();
     });
   });
+
+  // read more 
+
+  function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Lees meer";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Lees minder";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function readMore1() {
+    var dots1 = document.getElementById("dots1");
+    var moreText1 = document.getElementById("more1");
+    var btnText1 = document.getElementById("myBtn1");
+  
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btnText1.innerHTML = "Lees meer";
+      moreText1.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btnText1.innerHTML = "Lees minder";
+      moreText1.style.display = "inline";
+    }
+  }
+
+  function readMore2() {
+    var dots2 = document.getElementById("dots2");
+    var moreText2 = document.getElementById("more2");
+    var btnText2 = document.getElementById("myBtn2");
+  
+    if (dots2.style.display === "none") {
+      dots2.style.display = "inline";
+      btnText2.innerHTML = "Lees meer";
+      moreText2.style.display = "none";
+    } else {
+      dots2.style.display = "none";
+      btnText2.innerHTML = "Lees minder";
+      moreText2.style.display = "inline";
+    }
+  }
+
+  function readMore3() {
+    var dots3 = document.getElementById("dots3");
+    var moreText3 = document.getElementById("more3");
+    var btnText3 = document.getElementById("myBtn3");
+  
+    if (dots3.style.display === "none") {
+      dots3.style.display = "inline";
+      btnText3.innerHTML = "Lees meer";
+      moreText3.style.display = "none";
+    } else {
+      dots3.style.display = "none";
+      btnText3.innerHTML = "Lees minder";
+      moreText3.style.display = "inline";
+    }
+  }
+
+  function readMore4() {
+    var dots4 = document.getElementById("dots4");
+    var moreText4 = document.getElementById("more4");
+    var btnText4 = document.getElementById("myBtn4");
+  
+    if (dots4.style.display === "none") {
+      dots4.style.display = "inline";
+      btnText4.innerHTML = "Lees meer";
+      moreText4.style.display = "none";
+    } else {
+      dots4.style.display = "none";
+      btnText4.innerHTML = "Lees minder";
+      moreText4.style.display = "inline";
+    }
+  }
+
