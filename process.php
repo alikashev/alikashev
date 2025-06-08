4c487b824b68484808f6124e8631fe5a724940d1
Ali Kashev <kasev21@gmail.com>
2025-06-08 04:41:38 +0200

    mail testing

--- process.php
+++ process.php
@@ -27,8 +27,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = $_ENV['SMTP_PORT'];
 
-            echto $_ENV['SMTP_USER'];
-            echto $_ENV['SMTP_PORT'];
+            echo $_ENV['SMTP_USER'];
+            echo $_ENV['SMTP_PORT'];
 
             $mail->setFrom($_ENV['SMTP_USER'], 'Contactformulier');
             $mail->addAddress($_ENV['SMTP_USER']);
