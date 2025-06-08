304ab4d0302dd60d687c469d090483ded9501353
Ali Kashev <kasev21@gmail.com>
2025-06-08 03:49:38 +0200

    changed mailer script

--- process.php
+++ process.php
@@ -1,37 +1,44 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use Dotenv\Dotenv;
+
+require 'vendor/autoload.php';
+
+// .env laden
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Veilig de gegevens ophalen en filteren
     $naam = htmlspecialchars(trim($_POST["name"]));
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $onderwerp = htmlspecialchars(trim($_POST["subject"]));
     $bericht = htmlspecialchars(trim($_POST["formmessage"]));
 
-    echo $naam;
-    echo $email;
-    echo $onderwerp;
-    echo $bericht;
-
-
-    // Check of alles is ingevuld
     if (!empty($naam) && !empty($email) && !empty($onderwerp) && !empty($bericht)) {
-        $to = "contact@alikashev.nl"; // Verander dit naar je eigen e-mailadres
-        $subject = "Nieuw bericht van contactformulier";
-        
-        $message = "Naam: $naam\n";
-        $message .= "E-mail: $email\n";
-        $message .= "Onderwerp:\n$onderwerp\n";
-        $message .= "Bericht:\n$bericht\n";
+        $mail = new PHPMailer(true);
 
-        $headers = "From: contact@alikashev.nl\r\n";
-        $headers .= "Reply-To: $email\r\n";
-        $headers .= "X-Mailer: PHP/" . phpversion();
+        try {
+            $mail->isSMTP();
+            $mail->Host = $_ENV['SMTP_HOST'];
+            $mail->SMTPAuth = true;
+            $mail->Username = $_ENV['SMTP_USER'];
+            $mail->Password = $_ENV['SMTP_PASS'];
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = $_ENV['SMTP_PORT'];
 
-        // Verstuur de mail
-        if (mail($to, $subject, $message, $headers)) {
+            $mail->setFrom($_ENV['SMTP_USER'], 'Contactformulier');
+            $mail->addAddress($_ENV['SMTP_USER']);
+            $mail->addReplyTo($email, $naam);
+
+            $mail->isHTML(false);
+            $mail->Subject = "Nieuw bericht van contactformulier: $onderwerp";
+            $mail->Body = "Naam: $naam\nE-mail: $email\nOnderwerp: $onderwerp\nBericht:\n$bericht";
+
+            $mail->send();
             echo "Bedankt voor je bericht! We nemen zo snel mogelijk contact met je op.";
-        } else {
-            echo "Er is een fout opgetreden. Probeer het later opnieuw.";
+        } catch (Exception $e) {
+            echo "Er is een fout opgetreden bij het verzenden van de e-mail. Mailer Error: {$mail->ErrorInfo}";
         }
     } else {
         echo "Vul alle velden in aub.";
@@ -39,5 +46,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     echo "Ongeldige aanvraag.";
 }
-
 ?>
