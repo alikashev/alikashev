d3ee7789b18706f3a07042d1996528945523020a
Ali Kashev <kasev21@gmail.com>
2025-06-08 18:29:41 +0200

    changed contactform

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', (e)=>{
 
 //contact form 
 
-$(document).ready(function () {
+/* $(document).ready(function () {
     $("#form").submit(function (event) {
         $(".form-group").removeClass("has-error");
         $(".help-block").remove();
@@ -162,7 +162,7 @@ $(document).ready(function () {
       $recaptcha.setAttribute('required', 'required');
     }
   })
-
+*/
 
   // read more 
 
