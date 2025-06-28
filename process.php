cce93489538980e3a457919ded6c8c8837cf95e9
Ali Kashev <kasev21@gmail.com>
2025-06-28 23:03:47 +0200

    fixed

--- process.php
+++ process.php
@@ -27,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     if (empty($response['errors'])) {
-        $to = "contact@alikashev.nl";
+        $to = "ali.kashev@icloud.com";
         $subject = "Nieuw bericht van contactformulier";
 
         $message = "Naam: $naam\n";
